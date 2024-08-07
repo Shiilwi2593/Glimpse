b69c323bfd663f431a74391e547214f641375727
graylwi2593 <graylwi2593@gmail.com>
2024-08-07 15:53:38 +0700

    Initial Commit

---
+++ Yapping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yapping
+//
+//  Created by Trịnh Kiết Tường on 07/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
