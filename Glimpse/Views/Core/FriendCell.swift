f6fe8499add4fd41fa99da705d55709a538fafde
graylwi2593 <graylwi2593@gmail.com>
2024-09-14 22:50:15 +0700

    Friend list cell, friend status display

---
+++ Glimpse/Views/Core/FriendCell.swift
@@ -0,0 +1,23 @@
+//
+//  FriendCell.swift
+//  Glimpse
+//
+//  Created by Trịnh Kiết Tường on 14/09/2024.
+//
+
+import UIKit
+
+class FriendCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
